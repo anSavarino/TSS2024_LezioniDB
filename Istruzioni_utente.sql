9be40be37ba0b7f98646dce2acdfc68799aded83
anSavarino <angelo.savarino@edu.immaginazioneelavoro.it>
2024-02-07 16:23:56 +0100

    test git db

--- Istruzioni_utente.sql
+++ Istruzioni_utente.sql
@@ -16,3 +16,42 @@ create table if not exists studenti(
     ins timestamp default current_timestamp on update current_timestamp
     -- primary key(id)
 );
+-- istruzioni informative
+show tables;
+show create table studenti;
+desc studenti;
+show full columns from studenti;
+
+-- istruzioni di modifica
+alter table studenti rename to studente;
+rename table studente to studenti;
+
+alter table studenti modify cognome varchar(30) comment 'indice';
+alter table studenti comment 'anagrafica';
+
+alter table studenti
+modify cognome varchar(50);
+desc studenti;
+
+alter table studenti
+change nome nomes varchar(40);
+
+alter table studenti
+rename column nomes to nome;
+
+alter table studenti
+add eta tinyint unsigned;
+
+alter table studenti 
+modify eta tinyint after data_nascita;
+desc studenti;
+
+alter table studenti
+drop eta;
+
+alter table studenti
+add nazionalita char(3) first;
+
+alter table studenti
+modify nome varchar(30), add eta tinyint after data_nascita, drop nazionalita;
+
