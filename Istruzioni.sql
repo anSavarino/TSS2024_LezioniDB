016ed2986f95ae465580cbed5ee75509f8d7d0fe
anSavarino <angelo.savarino@edu.immaginazioneelavoro.it>
2024-02-09 14:01:05 +0100

    Lezione 09/02/24

--- Istruzioni.sql
+++ Istruzioni.sql
@@ -6,3 +6,6 @@ show warnings;
 create user 'app_tss'@'localhost' identified by 'tss_2024!';
 create user 'app_test'@'localhost' identified by 'test';
 grant all on tss2024.* to 'app_tss'@'localhost';
+
+create database if not exists java;
+grant all on java.* to 'app_tss'@'localhost';
--- Istruzioni_utente.sql
+++ Istruzioni_utente.sql
@@ -88,3 +88,147 @@ values(7, 'Pippo', 'Fava', 'm', 'via marche 3', 'Monza', 'mi', 'Lombardia', 'pfa
 
 insert into studenti
 values(default,'anna','rossi','f',null,'torino',default,default,'annarossi@gmail.com',null,default);
+desc studenti;
+
+-- Creare tabella amici
+create table if not exists amici(
+	id int auto_increment primary key,
+    nome varchar(30),
+    cognome varchar(50),
+    genere enum('m','f','nb')
+);
+
+-- id, nome, cognome, generestudenti
+insert into amici(nome, cognome, genere) select nome, cognome, genere from studenti;
+
+create table if not exists parenti(
+	id int auto_increment primary key,
+    nome varchar(30),
+    cognome varchar(50),
+    genere enum('m','f','nb')
+)select nome, cognome, genere from studenti;
+
+select * from parenti;
+
+create table studenti_bk like studenti; -- conserva la struttura della tabella
+insert into studenti_bk select * from studenti;
+
+create table studenti_bk2 as select * from studenti; -- non copia la struttura della tabella
+select * from studenti_bk2;
+
+desc studenti_bk2;
+
+-- UPDATE
+select*from studenti;
+
+update studenti set indirizzo = 'via roma 20', citta = 'torino'  where id = 3;
+
+select @@sql_mode;
+
+use tss2024;
+update studenti set provincia = 'mi' where id = 7;
+
+update studenti set provincia = 'to';
+
+update studenti set data_nascita = '2024-02-09';
+
+-- delete
+delete from studenti where id = 2;
+select * from studenti;
+
+delete from studenti where provincia = 'to';
+
+delete from studenti;
+
+insert into studenti(cognome, email) values ('vecchio', 'vecio@gmail.com');
+truncate studenti; -- drop table e ricrea
+
+select table_name, auto_increment 
+from information_schema.tables 
+where table_schema = 'tss2024';
+
+show table status like 'studenti';
+select * from studenti;
+
+truncate studenti;
+select * from studenti_bk;
+
+delete from studenti_bk;
+select * from studenti_bk;
+show table status like 'studenti_bk';
+alter table studenti_bk auto_increment = 1;
+insert into studenti_bk(cognome, email) values ('vecchio','vechio@gmail.com');
+
+-- ORDER BY
+select *
+from studenti
+order by cognome desc, nome desc;
+
+-- LIMIT
+SELECT 
+    *
+FROM
+    studenti
+ORDER BY cognome
+LIMIT 10 , 10; -- due parametri indice di partenza e numero di record
+
+SELECT 
+    nome,
+    cognome,
+    genere,
+    indirizzo,
+    citta,
+    provincia,
+    regione,
+    data_nascita
+FROM
+    studenti
+WHERE
+    genere = 'f'
+ORDER BY cognome;
+
+-- select -> from -> where -> order by -> limit
+
+-- ? != <>
+select * from studenti where provincia <> 'to';
+
+select * from studenti where data_nascita < '2000-01-01';
+-- and, or, not
+select * from studenti where genere = 'f' and provincia = 'to';
+select * from studenti where genere = 'f' or provincia = 'to';
+select * from studenti where not provincia != 'to';
+
+select * from studenti where provincia = 'to' or provincia = 'mi' or provincia = 'cn' or provincia = 'al';
+
+select * from studenti where cognome = 'verdi' or cognome = 'rossi' and provincia = 'to';
+SELECT 
+    *
+FROM
+    studenti
+WHERE
+    (cognome = 'rossi'
+        OR cognome = 'verdi') AND (provincia = 'to'
+        OR provincia = 'al');
+        
+-- in, not
+select * from studenti where provincia in ('mi','to','al','cn');
+
+-- between, not between
+
+SELECT 
+    *
+FROM
+    studenti
+WHERE
+    data_nascita NOT BETWEEN '1990-01-01' AND '1999-12-31'
+ORDER BY data_nascita;
+
+select * from studenti where cognome between 'a' and 'mz' order by cognome;
+
+-- is null, is not null
+
+select * from studenti where data_nascita is not null;
+
+-- like, not like
+select * from studenti where cognome like 'v%' order by cognome;
+select * from studenti where nome like '%a' order by nome;
